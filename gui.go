592709210f4147b552fd1625028f64ecf65be43d
Chris Casey <cdcasey@users.noreply.github.com>
2025-11-15 22:24:36 -0600

    Quit works

--- gui.go
+++ gui.go
@@ -45,5 +45,13 @@ func layout(g *gocui.Gui) error {
 }
 
 func keybindings(g *gocui.Gui) error {
+	// Quit on Ctrl+C
+	if err := g.SetKeybinding("", gocui.KeyCtrlC, gocui.ModNone, quit); err != nil {
+		return err
+	}
 	return nil
+}
+
+func quit(g *gocui.Gui, v *gocui.View) error {
+	return gocui.ErrQuit
 }
